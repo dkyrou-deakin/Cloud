e55eb256abae8d391ebe16f6a36ccb7d6fb9e6b2
dkyrou <dkyrou@deakin.edu.au>
2023-03-14 13:31:54 +1100

    first commit

---
+++ Week1/JSON/app4.js
@@ -0,0 +1,11 @@
+var express = require('express')
+var app = express()
+
+// respond with "hello world" when a GET request is madew to the hompeage
+app.get('/', function (req,res) {
+    res.send("Hello world")
+})
+
+//list to a particular port
+app.listen(3000)
+//VISIT http://localhost:3000/ to find the message
---
+++ Week1/app2.js
@@ -0,0 +1,11 @@
+var adder=function(first,second){
+    var sum=first+second
+    return sum
+}
+var log=function(msg){
+    console.log("[Log]: ", msg)
+}
+
+log("Hellow World")
+log("welcome to SIT 323/737")
+log("THe sum is " +adder(5,6))
